90a80608167bfadb712901e99ab1b6f1fe5e7f59
elru8cjo4@msn.com <carol_kao@trend.com.tw>
2016-01-11 17:56:28 +0800

    add dsminstall.prop file; modify echo Params script

--- echoParams.sh
+++ echoParams.sh
@@ -1,1 +1,30 @@
-echo $@ > /tmp/parameters
+getJsonValPre="['runtimeSettings'][0]['handlerSettings']['publicSettings']"
+
+function getJsonVal () {
+    python -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin
+)$1))";
+}
+
+function getParam() {
+    local key=$1
+    local paramValue=`cat $configFilePath | getJsonVal "$getJsonValPre['$key']
+" | sed 's/"//g'`
+    sed -i -e "s/%$key%/$paramValue/g" dsminstall.prop
+    eval $key=$paramValue
+}
+
+configFileNumber=`ls ../`
+configFilePath="../../config/"$configFileNumber".settings"
+
+
+getParam "adminUserName"
+getParam "adminPassword"
+getParam "databaseServer"
+getParam "databaseName"
+getParam "databaseUserName"
+getParam "databaseUserPassword"
+
+managerAddress=`curl http://ifconfig.co/?cmd=curl`
+sed -i -e "s/%managerAddress%/$managerAddress/g" dsminstall.prop
+
+/tmp/Manager-Linux-9.6.3175.x64.sh -q -console -varfile dsminstall.prop
