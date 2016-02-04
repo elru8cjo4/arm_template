2a04422b3abd623eac8d5f0e63a6bafb6307f812
Carol Kao (RD-TW) <carol_kao@trend.com.tw>
2016-02-04 15:22:34 +0800

    modify install property file path

--- installDSM.sh
+++ installDSM.sh
@@ -5,11 +5,14 @@ function getJsonVal () {
 )$1))";
 }
 
+installFilePath="/tmp/dsminstall.prop"
+installLogPath="/opt/trend/dsm_app/dsm_azure_app/azure_install.log"
+
 function getParam() {
     local key=$1
-    local paramValue=`cat $configFilePath | getJsonVal "$getJsonValPre['$key']
+    local paramValue=`cat $configFilePath | getJsonVal "$getJsonValPre['$key']   
 " | sed 's/"//g'`
-    sed -i -e "s/%$key%/$paramValue/g" dsminstall.prop
+    sed -i -e "s/%$key%/$paramValue/g" $installFilePath
     eval $key=$paramValue
 }
 
@@ -17,6 +20,7 @@ configFileNumber=`ls ../`
 configFilePath="../../config/"$configFileNumber".settings"
 cp $configFilePath /opt/trend/packages/dsm/default/0.settings
 
+cp dsminstall.prop $installFilePath
 getParam "adminUserName"
 getParam "adminPassword"
 getParam "databaseServer"
@@ -25,15 +29,14 @@ getParam "databaseUserName"
 getParam "databaseUserPassword"
 
 managerAddress=`curl http://ifconfig.co/?cmd=curl 2>/dev/null`
-sed -i -e "s/%managerAddress%/$managerAddress/g" dsminstall.prop
-cp dsminstall.prop /tmp/dsminstall.prop
+sed -i -e "s/%managerAddress%/$managerAddress/g" $installFilePath
 
 sleep 5
 
 r=1
 times=0
 while [ $r -ne 0 ] && [ $times -lt 3 ]; do
-    /opt/trend/packages/dsm/default/Manager-Azure-9.6.11857.x64.sh -q -console -varfile /tmp/dsminstall.prop > /tmp/dsmInstallOut 2>&1 
+    /opt/trend/packages/dsm/default/Manager-Azure-9.6.11857.x64.sh -q -console -varfile $installFilePath > $installLogPath 2>&1 
     r=$?
     (( times++ ))
     sleep 5
@@ -45,6 +48,5 @@ if [ $? != 0 ]; then
 fi
 
 #remove tmp prop file
-rm /tmp/dsminstall.prop > /dev/null 2>&1
-
+rm $installFilePath > /dev/null 2>&1
 exit 0
